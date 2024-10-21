d10c8bb3f695948300dc0e6f2e55118512db0411
Jason Luong <jluon366@mtroyal.ca>
2024-10-20 22:40:55 -0600

    changed the name of the folder to be able to submit the assignment. Final version of the assignment is called jluon366-Assignment

--- shared/header.php
+++ shared/header.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" href="/jluon366/css/header.css" />
+<link rel="stylesheet" href="/jluon366-Assignment/css/header.css" />
 <div class="wrapper">
   <header class="header">
       <div class="header-title">
